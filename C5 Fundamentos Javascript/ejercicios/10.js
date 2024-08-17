e32ffd266840f17afd4e0596b84ff9db74b1559e
David_Bucheli <dacuastumal@unimayor.edu.co>
2024-08-18 00:35:55 +1000

    I completed Fundamentals JS

--- C5 Fundamentos Javascript/ejercicios/10.js
+++ C5 Fundamentos Javascript/ejercicios/10.js
@@ -1,11 +1,5 @@
 function devolverString(string) {
-  // La función recibe un argumento llamado string.
-  // Debe retornar dicho string.
-  // Por ejemplo: 
-  // "texto" ---> "texto" 
-  // "hola mundo" ---> "hola mundo" 
-  // "SoyHenry" ---> "SoyHenry" 
-  // Tu código:
+  return string;
 }
 
-module.exports = devolverString;
+//module.exports = devolverString;
--- C5 Fundamentos Javascript/ejercicios/30.js
+++ C5 Fundamentos Javascript/ejercicios/30.js
@@ -7,6 +7,8 @@ function deEuroAdolar(euro) {
   // 1 ---> 1.20
   // 0 ---> 0
   // Tu código:
+  const tasaConversion = 1.20;
+  return euro * tasaConversion;
 }
 
 module.exports = deEuroAdolar;
